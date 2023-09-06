36aa3fb4984a2085b1e5c4c0b45b962f8e0bc908
sasholilov <alex_xld@abv.bg>
2023-09-06 14:36:11 +0300

    Update

---
+++ JS-Module1/2. ConditionalStatements/exercises.js
@@ -0,0 +1,10 @@
+//2.Check for an excellent rating
+function checkExcellentRating(rating) {
+  if (rating >= 5.5) {
+    console.log("Excellent");
+  } else console.log("no output");
+}
+checkExcellentRating(5.51);
+checkExcellentRating(4);
+
+//3.
