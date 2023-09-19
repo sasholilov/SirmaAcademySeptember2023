8be10a104aeb3a5c8de330e2b43c20bdbe452eb6
sasholilov <alex_xld@abv.bg>
2023-09-19 23:50:22 +0300

    Update

--- JS-Module1/6.Objects/exercises.js
+++ JS-Module1/6.Objects/exercises.js
@@ -68,3 +68,63 @@ function phonebook(arr) {
 console.log(
   phonebook(["Peter 0888 657 212", "Bob 0899 657 212", "Peter 123123"])
 );
+
+//6. Movie
+console.log("-------Movie--------");
+let inputArr = [
+  "addMovie Fast and Furious",
+  "addMovie Godfather",
+  "Inception directedBy Christopher Nolan",
+  "Godfather directedBy Francis Ford Coppola",
+  "Godfather onDate 29.07.2018",
+  "Fast and Furious onDate 30.07.2018",
+  " Spinderman 2 onDate 01.08.2018",
+  "Fast and Furious directedBy Rob Cohen",
+];
+
+let inputArr2 = [
+  "addMovie The Avengers",
+  "addMovie Spiderman",
+  "The Avengers directedBy Pesho Peshov",
+  "The Avengers onDate 30.07.2010",
+  "Captain America onDate 30.07.2010",
+  "Captain America directedBy Joe Russo",
+  "The Avengers directedBy Anthony Russo",
+];
+function movie(input) {
+  let addArray = input.filter((el) => el.includes("addMovie")).map((e) => e);
+  let directedByArray = input
+    .filter((el) => el.includes("directedBy"))
+    .map((e) => e);
+  let onDateArray = input.filter((el) => el.includes("onDate")).map((e) => e);
+  const movieArr = [];
+  for (let movie of addArray) {
+    movieArr.push({ movieName: movie.substring(movie.indexOf(" ") + 1) });
+  }
+  for (let i = 0; i < movieArr.length; i++) {
+    for (let j = 0; j < directedByArray.length; j++) {
+      let directedValues = directedByArray[j].split(" directedBy ");
+      if (movieArr[i].movieName == directedValues[0]) {
+        movieArr[i].directedBy = directedValues[1];
+      }
+    }
+  }
+  for (let i = 0; i < movieArr.length; i++) {
+    for (let j = 0; j < onDateArray.length; j++) {
+      let onDateValues = onDateArray[j].split(" onDate ");
+      if (movieArr[i].movieName == onDateValues[0]) {
+        movieArr[i].onDate = onDateValues[1];
+      }
+    }
+  }
+  for (let i = 0; i < movieArr.length; i++) {
+    if (
+      movieArr[i].hasOwnProperty("movieName") &&
+      movieArr[i].hasOwnProperty("directedBy") &&
+      movieArr[i].hasOwnProperty("onDate")
+    ) {
+      console.log(JSON.stringify(movieArr[i]));
+    }
+  }
+}
+movie(inputArr);
