c9ef02e793b9ada33ccadab6bd52ebbe1d71548d
sasholilov <alex_xld@abv.bg>
2023-09-22 01:16:56 +0300

    Update

--- JS-Module1/7.Classes/exercises.js
+++ JS-Module1/7.Classes/exercises.js
@@ -134,3 +134,71 @@ laptop2.turnOff();
 laptop2.turnOn();
 laptop2.turnOff();
 console.log(laptop2.isOn);
+
+//6.School Book
+console.log("-----School Book------");
+
+function schoolBook(input) {
+  const formatedData = [];
+  for (let data of input) {
+    const dataObj = {};
+    dataObj.grade = data.substring(
+      data.indexOf(",") + 9,
+      data.indexOf("Graduated") - 2
+    );
+    dataObj.name = data.substring(data.indexOf(":") + 2, data.indexOf(","));
+    dataObj.anualScore = data.substring(data.length - 4, data.length);
+    formatedData.push(dataObj);
+  }
+  const uniqueGrade = new Set();
+  for (let data of formatedData) {
+    uniqueGrade.add(data.grade);
+  }
+
+  let preFinalArr = [];
+  const finalArr = [];
+
+  for (let data of uniqueGrade) {
+    const newDataArray = formatedData.filter(
+      (e) => e.grade == data && e.anualScore > 2.99
+    );
+    preFinalArr = newDataArray.concat(preFinalArr);
+    console.log(preFinalArr);
+  }
+
+  const aggregatedStudents = {};
+  for (let data of preFinalArr) {
+    const { grade, name, anualScore } = data;
+    if (!aggregatedStudents[grade]) {
+      aggregatedStudents[grade] = {
+        name: [],
+        anualScore: [],
+      };
+    }
+    aggregatedStudents[grade].name.push(name);
+    aggregatedStudents[grade].anualScore.push(anualScore);
+  }
+
+  for (let [key, value] of Object.entries(aggregatedStudents)) {
+    console.log(Number(key) + 1, "Grade");
+    console.log(`List of students: ${value.name}`);
+    let valSum = value.anualScore.reduce((acc, e) => Number(acc) + Number(e));
+    console.log((Number(valSum) / Number(value.name.length)).toFixed(2));
+    console.log("===");
+  }
+}
+
+schoolBook([
+  "Student name: Mark, Grade: 8, Graduated with an average score: 4.75",
+  "Student name: Ethan, Grade: 9, Graduated with an average score: 5.66",
+  "Student name: George, Grade: 8, Graduated with an average score: 2.83",
+  "Student name: Steven, Grade: 10, Graduated with an average score: 4.20",
+  "Student name: Joey, Grade: 9, Graduated with an average score: 4.90",
+  "Student name: Angus, Grade: 11, Graduated with an average score: 2.90",
+  "Student name: Bob, Grade: 11, Graduated with an average score: 5.15",
+  "Student name: Daryl, Grade: 8, Graduated with an average score: 5.95",
+  "Student name: Bill, Grade: 9, Graduated with an average score: 6.00",
+  "Student name: Philip, Grade: 10, Graduated with an average score: 5.05",
+  "Student name: Peter, Grade: 11, Graduated with an average score: 4.88",
+  "Student name: Gavin, Grade: 10, Graduated with an average score: 4.00",
+]);
