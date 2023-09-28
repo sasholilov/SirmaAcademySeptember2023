dd0dbe546784ad020b4f99941af91c61f85ee6ab
sasholilov <alex_xld@abv.bg>
2023-09-28 21:59:44 +0300

    Update

--- JS-Module1/11.Strings/exercises.js
+++ JS-Module1/11.Strings/exercises.js
@@ -55,3 +55,77 @@ hashTag("Everyone uses # to tag a #special word in #facebook");
 hashTag(
   "The symbol # is known #variously in English-speaking #regions as the #number sign"
 );
+
+//6. Extract File
+console.log("--------Extract File--------");
+
+function extractFile(text) {
+  let indexSlash = text.lastIndexOf("\\") + 1;
+  let fileNameWithExtension = text.slice(indexSlash);
+  let filename = fileNameWithExtension.slice(
+    0,
+    fileNameWithExtension.lastIndexOf(".")
+  );
+  let extension = fileNameWithExtension.slice(
+    fileNameWithExtension.lastIndexOf(".") + 1
+  );
+  console.log(fileNameWithExtension);
+  console.log(`File name: ${filename}`);
+  console.log(`File extension: ${extension}`);
+}
+
+extractFile("C:\\desktop\\academy\\template.pptx");
+extractFile("C:\\Projects\\website.folder\\file.name.js");
+
+//7. Substring
+console.log("---------Substring----------");
+function mySubstringFunc(word, text) {
+  const isFound = text
+    .toLowerCase()
+    .split(" ")
+    .some((e) => e === word);
+  if (!isFound) console.log(`${word} is not found!`);
+  else console.log(`${word} is not found!`);
+}
+
+mySubstringFunc("javascript", "JavaScript is the best programming language");
+mySubstringFunc("python", "JavaScript is the best programming language");
+
+//8. Replace Repeating Symbols
+console.log("---------Replace Repeating Symbols-----------");
+function replaceRepaetingSymbols(text) {
+  let newStr = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] !== text[i + 1]) {
+      newStr += text[i];
+    }
+  }
+  console.log(newStr);
+}
+
+replaceRepaetingSymbols("aaaaabbbbbcdddeeeedssaa");
+replaceRepaetingSymbols("qqqwerqwecccwd");
+
+//9. Pascal-Case Spliter
+console.log("---------Pascal-Case Spliter---------");
+function pascalCaseSpliter(text) {
+  console.log(text.split(/(?=[A-Z])/));
+}
+pascalCaseSpliter("SplitMeIfYouCan");
+
+//10. Cunt And Reverse
+console.log("--------Cut And Reverse---------");
+function cutReverse(text) {
+  let str3 = "";
+  let str4 = "";
+  let str1 = text.slice(0, text.length / 2);
+  let str2 = text.slice(text.length / 2);
+  for (let i = text.length / 2 - 1; i >= 0; i--) {
+    str3 += str1[i];
+    str4 += str2[i];
+  }
+  console.log(str3);
+  console.log(str4);
+}
+cutReverse("tluciffiDsIsihTgnizamAoSsIsihT");
+cutReverse("sihToDtnaCuoYteBIboJsihTtAdooGoSmI");
