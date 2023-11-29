c69a78f648427ee54f121eb32cea2d1413d84bbe
sasholilov <alex_xld@abv.bg>
2023-11-29 22:48:05 +0200

    react exercises

---
+++ JS-Module3/react-exercises/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="header">
+      <h1>My first app</h1>
+    </div>
+  );
+}
+
+export default App;
