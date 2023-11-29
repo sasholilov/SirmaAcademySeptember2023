d0413ae6b9708f26780f5813083bced343df8d0a
sasholilov <alex_xld@abv.bg>
2023-11-29 23:11:12 +0200

    ex

--- JS-Module3/react-exercises/src/App.js
+++ JS-Module3/react-exercises/src/App.js
@@ -1,10 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+const person = {
+  name: "John Doe",
+  age: 30,
+  address: {
+    street: "123 Main St",
+    city: "Cityville",
+    zip: "12345",
+  },
+};
+
+const formatedPerson = {
+  ...person,
+  address: Object.values(person.address).join(", "),
+};
+
 function App() {
   return (
     <div className="header">
-      <h1>My first app</h1>
+      <div className="data-header">
+        {Object.keys(person).map((e) => (
+          <h4>{e}</h4>
+        ))}
+      </div>
+      <div className="data-values">
+        {Object.values(formatedPerson).map((value) => (
+          <h4>{value}</h4>
+        ))}
+      </div>
+      <h2></h2>
     </div>
   );
 }
