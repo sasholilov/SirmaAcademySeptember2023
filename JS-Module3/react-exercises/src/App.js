a5ca4f873a1dc94d6d9b381ff538ed10794a59ac
sasholilov <alex_xld@abv.bg>
2023-12-04 21:17:39 +0200

    clear old exercise and prepare to build homework. Set to load new csv file with basketball data

--- JS-Module3/react-exercises/src/App.js
+++ JS-Module3/react-exercises/src/App.js
@@ -3,37 +3,8 @@ import "./App.css";
 
 const reader = new FileReader();
 
-const person = {
-  name: "John Doe",
-  age: 30,
-  address: {
-    street: "123 Main St",
-    city: "Cityville",
-    zip: "12345",
-  },
-};
-
-const formatedPerson = {
-  ...person,
-  address: Object.values(person.address).join(", "),
-};
-
-function Button({ setNext, value }) {
-  return <button onClick={() => setNext(!value)}>Next</button>;
-}
-
 function App() {
-  const [facts, setFacts] = useState([]);
-  const [next, setNext] = useState(false);
-  const [text, setText] = useState([]);
-
-  useEffect(() => {
-    fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
-      .then((response) => response.json())
-      .then((data) => {
-        setFacts(data);
-      });
-  }, [next]);
+  const [data, setdata] = useState([]);
 
   function handleFileChange(e) {
     const file = e.target.files[0];
@@ -42,54 +13,59 @@ function App() {
     reader.onload = function () {
       const data = reader.result
         .split(/(\r\n|\r|\n)/g)
-        .filter((value) => value.length > 4);
-      console.log(data);
-
+        .filter((value) => value.length > 2);
       const newDataArray = data.map((e) => e.split(";"));
+
       const newDataArrayWithObj = newDataArray.map((row) => {
         return {
-          productId: row[0],
-          productName: row[1],
-          productModels: row[2],
+          playerName: row[0],
+          team: row[1],
+          timePlayed: row[2],
+          score: row[3],
         };
       });
+      setdata(newDataArrayWithObj);
+      //const sorterdArray = newDataArrayWithObj.sort(
+      //  (a, b) => b.score - a.score
+      //);
+      //console.log("sortirano", sorterdArray);
       console.log(newDataArrayWithObj);
-      setText(newDataArrayWithObj);
     };
   }
 
   return (
     <div>
-      <div className="header">
-        <div className="data-header">
-          <h1>{facts.text}</h1>
-          <h2>Source: {facts.source}</h2>
-          <Button value={next} setNext={setNext} />
-        </div>
-      </div>
-      {text.length > 0 && (
-        <div className="file-exercise">
-          <div className="title">
-            <p>{text[0].productId}</p>
-            <p>{text[0].productName}</p>
-            <p>{text[0].productModels}</p>
-          </div>
-          <div className="title">
-            {text
-              .map((e) => (
-                <div className="title-results">
-                  <p>{e.productId}</p>
-                  <p>{e.productName}</p>
-                  <p>{e.productModels}</p>
-                </div>
-              ))
-              .filter((e, index) => index > 0)}
-          </div>
+      {data.length > 0 && (
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>{data[0].playerName}</th>
+                <th>{data[0].team}</th>
+                <th>{data[0].timePlayed}</th>
+                <th>{data[0].score}</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data
+                .filter((e, index) => index > 0)
+                .map((e, index) => (
+                  <tr key={index}>
+                    <td>
+                      {index + 1}. {e.playerName}
+                    </td>
+                    <td>{e.team}</td>
+                    <td>{e.timePlayed}</td>
+                    <td>{e.score}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
         </div>
       )}
-      <input type="file" onChange={handleFileChange}></input>
+
+      <input type="file" onChange={handleFileChange} />
     </div>
   );
 }
-
 export default App;
