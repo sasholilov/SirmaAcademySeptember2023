b60848e9d689b757234eedeb0dfb4dba3d8efd16
sasholilov <alex_xld@abv.bg>
2023-12-06 01:32:08 +0200

    added h2 title in pointsByTeam component

--- JS-Module3/react-exercises/src/components/tablePointsByTeam/pointsByTeam.jsx
+++ JS-Module3/react-exercises/src/components/tablePointsByTeam/pointsByTeam.jsx
@@ -4,24 +4,27 @@ const TablePointsByTeam = ({ data }) => {
   const newData = pointsByTeamData(data);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Класиране</th>
-          <th>Отбор</th>
-          <th>Отбелязани точки</th>
-        </tr>
-      </thead>
-      <tbody>
-        {newData.map((e, index) => (
-          <tr key={index}>
-            <td>{index + 1}</td>
-            <td>{e.team}</td>
-            <td>{e.score}</td>
+    <div>
+      <h2>Общо вкарани точки по отбори във всички мачове</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Класиране</th>
+            <th>Отбор</th>
+            <th>Отбелязани точки</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {newData.map((e, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{e.team}</td>
+              <td>{e.score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
