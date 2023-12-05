4143133a72bc7e2924709c1c5377aa957d94bf72
sasholilov <alex_xld@abv.bg>
2023-12-05 21:24:43 +0200

    remove heading from the file and read the data from 1-st line

--- JS-Module3/react-exercises/src/components/tableData/tableData.jsx
+++ JS-Module3/react-exercises/src/components/tableData/tableData.jsx
@@ -2,18 +2,16 @@ import("./tableData.css");
 const TableData = ({ data }) => {
   return (
     <tbody>
-      {data
-        .filter((e, index) => index > 0)
-        .map((e, index) => (
-          <tr key={index}>
-            <td>
-              {index + 1}. {e.playerName}
-            </td>
-            <td>{e.team}</td>
-            <td>{e.timePlayed}</td>
-            <td>{e.score}</td>
-          </tr>
-        ))}
+      {data.map((e, index) => (
+        <tr key={index}>
+          <td>
+            {index + 1}. {e.playerName}
+          </td>
+          <td>{e.team}</td>
+          <td>{e.timePlayed}</td>
+          <td>{e.score}</td>
+        </tr>
+      ))}
     </tbody>
   );
 };
